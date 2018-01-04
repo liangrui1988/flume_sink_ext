a830661819e80210bac42adb9c380b1badc03e27
ruiliang <liangrui_1988@126.com>
2018-01-04 17:16:06 +0800

    分表语句

--- doc/sql/v1/pg_sql.sql
+++ doc/sql/v1/pg_sql.sql
@@ -19,18 +19,26 @@ CREATE INDEX "zl_log_info_main_time_log_index" ON "zl"."zl_log_info_main" USING 
 CREATE INDEX "zl_log_info_main_uuid_index" ON "zl"."zl_log_info_main" USING btree ("uuid" "pg_catalog"."int8_ops");
 
 -- 分表函数
-
 CREATE OR replace FUNCTION zl.fn_create_table_log2 (yearx int) RETURNS integer
  AS $$  
 declare aa integer;
 declare ret integer;
 declare table_ text;
+declare dt1 text;
+declare dt2 text;
 BEGIN    
    FOR aa IN 1..12 LOOP     
     table_:='zl_log_info'||yearx||'_'||aa;
+    dt1:=yearx||'-'||aa||'-01';
+    dt2:=yearx||'-'||(aa+1)||'-01';
+    --跨年
+		if aa=12 then
+			dt2:=(yearx+1)||'-01-01';
+		end if;   
      -- raise notice '%',table_; 
 			-- 建表语句开始
-			EXECUTE format('CREATE TABLE if not exists "%1$s" ("id" int8 DEFAULT nextval(''zl.seq_zl_log_info_main_id''::regclass) NOT NULL,"server_id" int4,"cont" jsonb,"time" timestamp(6),"file" varchar(150) COLLATE "default","time_log" int8 DEFAULT 0,	"uuid" int8 DEFAULT 0,PRIMARY KEY ("id"))	INHERITS ("zl_log_info_main")WITH (OIDS=FALSE) TABLESPACE game_log_space',table_);	
+			-- EXECUTE format('CREATE TABLE if not exists "%1$s" ("id" int8 DEFAULT nextval(''zl.seq_zl_log_info_main_id''::regclass) NOT NULL,"server_id" int4,"cont" jsonb,"time" timestamp(6),"file" varchar(150) COLLATE "default","time_log" int8 DEFAULT 0,	"uuid" int8 DEFAULT 0,PRIMARY KEY ("id"))	INHERITS ("zl_log_info_main")WITH (OIDS=FALSE) TABLESPACE game_log_space',table_);	
+     	EXECUTE format('CREATE TABLE if not exists "%1$s" (CHECK (time >= DATE ''%2$s'' AND time< DATE ''%3$s'')) INHERITS (zl_log_info_main) WITH (OIDS=FALSE) TABLESPACE game_log_space',table_,dt1,dt2);	
       EXECUTE format('ALTER TABLE "%1$s" OWNER TO "game"',table_); 
       EXECUTE format('CREATE INDEX "%1$s_file_index" ON "%1$s" USING btree ("file" "pg_catalog"."text_ops")',table_);
       EXECUTE format('CREATE INDEX "%1$s_server_index" ON "%1$s" USING btree ("server_id" "pg_catalog"."int4_ops")',table_); 
@@ -48,7 +56,8 @@ ALTER FUNCTION zl.fn_create_table_log2(yearx int)
     OWNER TO game;
 COMMENT ON FUNCTION zl.fn_create_table_log2(yearx int)
     IS '建一年数据分表语句';
-    
+
+select zl.fn_create_table_log2(2018);  
     -- 测试drop
 drop table zl.zl_log_info2018_1;
 drop table zl.zl_log_info2018_2;
